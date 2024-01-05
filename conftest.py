7fca5cf37d1ea69ab0d8210229cd3411965fd861
raphaelsanti <raphaell.solo@gmail.com>
2024-01-05 21:51:39 +0500

    Allure фикс

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import pytest
 
 
 @pytest.fixture()
 def browser():
-    # options = Options()
     driver = webdriver.Chrome()
     driver.maximize_window()
     yield driver
